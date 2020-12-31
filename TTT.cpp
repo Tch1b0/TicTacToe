aba508835f88e65d35ad6ea157f36a16ade34629
Tch1b0 <Jpour3006@gmail.com>
2020-12-31 12:18:17 +0100

    Added a single player mode

--- TTT.cpp
+++ TTT.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iterator>
+#include <time.h>
 using namespace std;
 
 void drawField(string field[3][3]){
@@ -19,11 +20,14 @@ string checkField(string field[3][3]){
     string result = "0";
     bool freefields = false;
     for (int i=0; i < 3; i++){
+        if (field[pos1][0] == field[pos1][1] && field[pos1][1] == field[pos1][2] && field[pos1][1] != " "){
+            result = field[pos1][1];
+        }
         for (int n=0; n < 3; n++){
-            if (pos2 != 0 && pos2 != 2 && field[pos1][pos2-1] == field[pos1][pos2] && field[pos1][pos2] == field[pos1][pos2+1] && field[pos1][pos2] != " "){
-                result = field[pos1][pos2];
+            if (field[0][pos2] == field[1][pos2] && field[1][pos2] == field[2][pos2] && field[1][pos2] != " "){
+                result = field[1][pos2];
             }
-            else if (pos1 != 0 && pos1 != 3 && field[pos1-1][pos2] == field[pos1][pos2] && field[pos1][pos2] == field[pos1+1][pos2] && field[pos1][pos2] != " "){
+            else if (field[pos1-1][pos2] == field[pos1][pos2] && field[pos1][pos2] == field[pos1+1][pos2] && field[pos1][pos2] != " "){
                 result = field[pos1][pos2];
             }
             else if (field[0][0] == field[1][1] && field[1][1] == field[2][2] && field[1][1] != " " || field[0][2] == field[1][1] && field[2][0] == field[1][1] && field[1][1] != " "){
@@ -38,45 +42,76 @@ string checkField(string field[3][3]){
     return result;
 }
 
+//void computerTurn(){}
+
 int main() {
     string field[3][3] = {
         {" ", " ", " "},
         {" ", " ", " "},
         {" ", " ", " "}
     };
-
     bool run = true;
     int pos1;
     int pos2;
+    int cpos1;
+    int cpos2;
     bool round = false;
     string player;
     string winner;
+    string choice;
+    string* fieldpointer0 = field[0];
+    string* fieldpointer1 = field[1];
+    string* fieldpointer2 = field[2];
+
+    do{
+        cout << "Do you want to play against" << endl << "another (p)layer or the (c)omputer?" << endl;
+        cin >> choice;
+        system("cls");
+    }while(choice != "p" && choice != "c");
+
     while(run){
-        drawField(field);
+        if (choice == "p" || choice == "c" && round == false){
+            drawField(field);
+        }
 
         if (round == false) player = "O";
-        else player = "X";
-        round = !round;
+        else if(round == true && choice == "p") player = "X";
+        else if (choice == "c" && round == true){
+            int counter = 0;
+            srand(time(NULL));
+            do{
+                cpos1 = rand() % 3;
+                cpos2 = rand() % 3;
+                counter++;
+            }while(field[cpos1][cpos2] != " " && counter < 100);
+            if (counter < 100){
+                field[cpos1][cpos2] = "X";
+            }
+        }
 
         winner = checkField(field);
-        if (winner == "0"){
+        if (round == false && winner == "0" || round == true && choice != "c" && winner == "0"){
             do{
             cout << "Player " << player << endl;
-            cout << "Y-axis: ";
+            cout << "y-axis: ";
             cin >> pos1;
-            cout << "X-axis: ";
+            cout << "x-axis: ";
             cin >> pos2;
             }while(field[pos1-1][pos2-1] != " ");
 
             field[pos1-1][pos2-1] = player;
             cout << endl;
         }else if (winner == "-") {
+            system("cls");
+            drawField(field);
             system("color 4");
             cout << "Tie" << endl;
             system("pause");
             exit(0);
 
-        }else{
+        }else if (winner == "O" || winner == "X"){
+            system("cls");
+            drawField(field);
             system("color 2");
             cout << "Player " << winner << " won!" << endl;
             system("pause");
@@ -86,6 +121,7 @@ int main() {
         if(field[0][0] != "O" && field[0][0] != "X" && field[0][0] != " "){ 
             field[0][0] = " ";
         }
+        round = !round;
         system("cls");
     }
 }
